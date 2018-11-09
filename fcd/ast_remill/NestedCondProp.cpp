e1691f77dfd63e76ef189aea68804412ab920c04
Marek Surovič <marek.surovic@gmail.com>
2018-11-09 15:02:51 +0100

    Adds condition propagation to nested else statements

--- fcd/ast_remill/NestedCondProp.cpp
+++ fcd/ast_remill/NestedCondProp.cpp
@@ -18,9 +18,24 @@
 #include <glog/logging.h>
 
 #include "fcd/ast_remill/NestedCondProp.h"
+#include "fcd/ast_remill/Util.h"
 #include "fcd/ast_remill/Z3ConvVisitor.h"
 
 namespace fcd {
+
+namespace {
+
+static std::vector<clang::IfStmt *> GetIfStmts(clang::CompoundStmt *compound) {
+  std::vector<clang::IfStmt *> result;
+  for (auto stmt : compound->body()) {
+    if (auto ifstmt = llvm::dyn_cast<clang::IfStmt>(stmt)) {
+      result.push_back(ifstmt);
+    }
+  }
+  return result;
+}
+
+}  // namespace
 
 char NestedCondProp::ID = 0;
 
@@ -34,14 +49,12 @@ NestedCondProp::NestedCondProp(clang::CompilerInstance &ins,
 
 bool NestedCondProp::VisitIfStmt(clang::IfStmt *ifstmt) {
   // DLOG(INFO) << "VisitIfStmt";
-  auto cond = ifstmt->getCond();
-  auto then = ifstmt->getThen();
   // Retrieve a parent `clang::IfStmt` condition
   // and remove it from `cond` if it's present.
   auto iter = parent_conds.find(ifstmt);
   if (iter != parent_conds.end()) {
-    auto child_expr = z3_gen->GetOrCreateZ3Expr(cond);
-    auto parent_expr = z3_gen->GetOrCreateZ3Expr(iter->second);
+    auto child_expr = z3_gen->GetOrCreateZ3Expr(ifstmt->getCond()).simplify();
+    auto parent_expr = z3_gen->GetOrCreateZ3Expr(iter->second).simplify();
     z3::expr_vector src(*z3_ctx);
     z3::expr_vector dst(*z3_ctx);
     src.push_back(parent_expr);
@@ -50,17 +63,27 @@ bool NestedCondProp::VisitIfStmt(clang::IfStmt *ifstmt) {
     ifstmt->setCond(z3_gen->GetOrCreateCExpr(sub));
   }
   // Determine whether `cond` is a constant expression
+  auto cond = ifstmt->getCond();
   if (!cond->isIntegerConstantExpr(*ast_ctx)) {
+    auto stmt_then = ifstmt->getThen();
+    auto stmt_else = ifstmt->getElse();
     // `cond` is not a constant expression and we propagate it
     // to `clang::IfStmt` nodes in it's `then` branch.
-    if (auto comp = llvm::dyn_cast<clang::CompoundStmt>(then)) {
-      for (auto stmt : comp->body()) {
-        if (auto child = llvm::dyn_cast<clang::IfStmt>(stmt)) {
-          parent_conds[child] = cond;
-        }
+    if (auto comp = llvm::dyn_cast<clang::CompoundStmt>(stmt_then)) {
+      for (auto child : GetIfStmts(comp)) {
+        parent_conds[child] = cond;
       }
     } else {
       LOG(FATAL) << "Then branch must be a clang::CompoundStmt!";
+    }
+    if (stmt_else) {
+      if (auto comp = llvm::dyn_cast<clang::CompoundStmt>(stmt_else)) {
+        for (auto child : GetIfStmts(comp)) {
+          parent_conds[child] = CreateNotExpr(*ast_ctx, cond);
+        }
+      } else {
+        LOG(FATAL) << "Else branch must be a clang::CompoundStmt!";
+      }
     }
   }
   return true;
